b9bf2da93bae6f4d0d512a695ceedad28572774e
Amit Rathore <amitrathore@gmail.com>
2009-02-02 23:19:55 -0800

    cleaning up the configuration specification a little

--- spec_suite.clj
+++ spec_suite.clj
@@ -2,50 +2,33 @@
 (load-file "spec/org/rathore/amit/capjure_spec.clj")
 
 (in-ns 'capjure-spec)
+(load-file "src/org/rathore/amit/init_util.clj")
 (import '(java.net URLEncoder URLDecoder))
-(println "**************** running tests ******************")
 
-(defstruct key-config :qualifier :functor)
-(defn add-key-config [keys-config name qualifier functor]
-  (assoc keys-config name (struct key-config qualifier functor)))
-(defn encode-keys []
-  (let [keys-config {}
-	keys-config (add-key-config keys-config 
-				    :inserts :merchant_product_id 
-				    (fn [insert-map]
-				      (insert-map :merchant_product_id)))
-	keys-config (add-key-config keys-config 
-				    :latest_consumer :merchant_id 
-				    (fn [consumer-map]
-				      (consumer-map :merchant_id)))
-	keys-config (add-key-config keys-config 
-				    :cart_items 
-				    :merchant_product_id (fn [cart-item-map]
-							   (let [sku (cart-item-map :sku)
-								 m-pid (cart-item-map :merchant_product_id)]
-							     (cond
-							      (nil? sku) m-pid
-							      :else (str m-pid "@" (URLEncoder/encode sku "UTF-8"))))))]
-    keys-config))
+(def encoders (config-keys
+  (config-for :inserts :merchant_product_id  (fn [insert-map]
+					       (insert-map :merchant_product_id)))
+  (config-for :latest_consumer :merchant_id (fn [consumer-map]
+					      (consumer-map :merchant_id)))
+  (config-for :cart_items  :merchant_product_id (fn [cart-item-map]
+						  (let [sku (cart-item-map :sku)
+							m-pid (cart-item-map :merchant_product_id)]
+						    (cond
+						     (nil? sku) m-pid
+						     :else (str m-pid "@" (URLEncoder/encode sku "UTF-8"))))))))
 
-(defn decode-keys []
-  (let [keys-config {}
-	keys-config (add-key-config keys-config 
-				    :inserts :merchant_product_id 
-				    (fn [value] value))
-	keys-config (add-key-config keys-config 
-				    :latest_consumer :merchant_id 
-				    (fn [value] value))
-	keys-config (add-key-config keys-config 
-				    :cart_items 
-				    :merchant_product_id (fn [value]
-							   (cond
-							      (.contains value "@") (first (.split (URLDecoder/decode value "UTF-8") "@"))
-							      :else value)))]
-    keys-config))
+(def decoders (config-keys
+  (config-for :inserts :merchant_product_id 
+	      (fn [value] value))
+  (config-for :latest_consumer :merchant_id 
+	      (fn [value] value))
+  (config-for :cart_items :merchant_product_id 
+	      (fn [value]
+		(cond
+		 (.contains value "@") (first (.split (URLDecoder/decode value "UTF-8") "@"))
+		 :else value)))))
 
-(def keys-config {:encode (encode-keys) :decode (decode-keys)})
-
+(def keys-config {:encode encoders :decode decoders})
 (binding [*hbase-master* "tank.cinchcorp.com:60000"
 	  *primary-keys-config* keys-config]
   (run-tests)
